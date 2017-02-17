7ce76c5cc8853983af9e8cfea4b328bc2e97a833
Miguel Sánchez de León Peque <peque@neosit.es>
2017-02-17 20:49:16 +0100

    Add wall and Raspberry Pi mount and hole

--- stowaway/top.py
+++ stowaway/top.py
@@ -23,59 +23,103 @@ MESH_WIDTH = 6
 MESH_LENGTH = 4
 
 # CKIT dimensions
-CKIT_WIDTH = 99
-CKIT_LENGTH = 63
-CKIT_CLEAR = 15
-CKIT_INNER = 2
-CKIT_OUTTER = 5
-CKIT_HEIGHT = 20  # TODO: calculate this
-CKIT_CENTER_WIDTH = (WIDTH - CKIT_WIDTH) / 2. - CKIT_CLEAR
-CKIT_CENTER_LENGTH = (LENGTH - CKIT_LENGTH) / 2. - CKIT_CLEAR
+CKIT = dict(
+    WIDTH=99,
+    LENGTH=63,
+    CLEAR=15,
+    INNER=2,
+    OUTTER=5,
+    HEIGHT=20  # TODO: calculate this,
+)
+CKIT['CENTER_WIDTH'] = (WIDTH - CKIT['WIDTH']) / 2. - CKIT['CLEAR']
+CKIT['CENTER_LENGTH'] = (LENGTH - CKIT['LENGTH']) / 2. - CKIT['CLEAR']
+
+# Raspberry Pi dimensions
+RBPI_HOLE_WIDTH = 56
+RBPI_HOLE_HEIGHT = 20
+RBPI = dict(
+    WIDTH=85,
+    LENGTH=49,
+    CLEAR=27,
+    INNER=2.75,
+    OUTTER=6,
+    HEIGHT=BOTTOM_HEIGHT-RBPI_HOLE_HEIGHT
+)
+RBPI['CENTER_WIDTH'] = (WIDTH - RBPI['WIDTH']) / 2. - RBPI['CLEAR']
+RBPI['CENTER_LENGTH'] = -((LENGTH - RBPI['LENGTH']) / 2. - RBPI['CLEAR'])
+
+# Wall dimensions
+WALL_THICK = THICK
+WALL_CENTER_WIDTH = min(
+    RBPI['CENTER_WIDTH'] - RBPI['WIDTH'] / 2. - RBPI['CLEAR'],
+    CKIT['CENTER_WIDTH'] - CKIT['WIDTH'] / 2. - CKIT['CLEAR']
+)
+WALL_HEIGHT = BOTTOM_HEIGHT - 5
 
 
-def passthrough_vertices(face):
+def enclosure_passthrough_vertices(face):
     workplane = face.workplane()
     rect = workplane.rect(PASS_WIDTH, PASS_LENGTH, forConstruction=True)
     return rect.vertices()
 
 
-def enclosure_part(height):
+def enclosure(height):
     # Bottom enclosure part
     part = Workplane('XY').box(WIDTH, LENGTH, height)\
         .faces('+Z').shell(THICK)
-    part = passthrough_vertices(part.faces('>Z'))\
+    part = enclosure_passthrough_vertices(part.faces('>Z'))\
         .rect(PASS_OUTTER, PASS_OUTTER).extrude(-height)\
         .edges('|Z').fillet(2)
-    part = passthrough_vertices(part.faces('<Z'))\
+    part = enclosure_passthrough_vertices(part.faces('<Z'))\
         .cboreHole(PASS_INNER, PASS_CBORE, PASS_CBORE_DEPTH)
     return part
 
 
-# Bottom enclosure
-bottom = enclosure_part(BOTTOM_HEIGHT)
+def mount_vertices(face, dimm):
+    return face.workplane()\
+        .center(dimm['CENTER_WIDTH'], dimm['CENTER_LENGTH'])\
+        .rect(dimm['WIDTH'], dimm['LENGTH'], forConstruction=True).vertices()
 
-# CKIT mount
+
+def mount(part, dimm):
+    vertices = mount_vertices(part.faces('<Z'), dimm)
+    part = vertices\
+        .circle(dimm['OUTTER'] / 2.).extrude(-THICK - dimm['HEIGHT'])
+    part = vertices\
+        .cboreHole(dimm['INNER'], dimm['OUTTER'], PASS_CBORE_DEPTH)
+    return part
+
+
+# Bottom enclosure
+bottom = enclosure(BOTTOM_HEIGHT)
+
+# CKIT and Raspberry Pi mounts
+bottom = mount(bottom, CKIT)
+bottom = mount(bottom, RBPI)
+
+# Raspberry Pi hole
+bottom = bottom.faces('>X').workplane()\
+    .center(-RBPI['CENTER_LENGTH'], (BOTTOM_HEIGHT - RBPI_HOLE_HEIGHT) / 2.)\
+    .rect(RBPI_HOLE_WIDTH, RBPI_HOLE_HEIGHT).cutBlind(-THICK)
+
+# Wall
 bottom = bottom.faces('<Z').workplane()\
-    .center(CKIT_CENTER_WIDTH, CKIT_CENTER_LENGTH)\
-    .rect(CKIT_WIDTH, CKIT_LENGTH, forConstruction=True).vertices()\
-    .circle(CKIT_OUTTER / 2.).extrude(-THICK - CKIT_HEIGHT)
-bottom = bottom.faces('<Z').workplane()\
-    .center(CKIT_CENTER_WIDTH, CKIT_CENTER_LENGTH)\
-    .rect(CKIT_WIDTH, CKIT_LENGTH, forConstruction=True).vertices()\
-    .cboreHole(CKIT_INNER, CKIT_OUTTER, PASS_CBORE_DEPTH)
+    .center(WALL_CENTER_WIDTH, 0)\
+    .rect(WALL_THICK, LENGTH)\
+    .extrude(-WALL_HEIGHT - THICK)
 
 
 # Top enclosure
-top = enclosure_part(TOP_HEIGHT)
+top = enclosure(TOP_HEIGHT)
 top = top.mirror('XY')
 
 # CKIT mesh
 top = top.faces('<Z').workplane()\
-    .center(CKIT_CENTER_WIDTH, CKIT_CENTER_LENGTH)\
+    .center(CKIT['CENTER_WIDTH'], CKIT['CENTER_LENGTH'])\
     .rarray(MESH_SPACE, MESH_SPACE, MESH_WIDTH, MESH_LENGTH, center=False)\
     .rect(8, 8).cutThruAll()
 
-top = top.translate((200, 0, 0))
+top = top.translate((WIDTH + 20, 0, 0))
 
 
 # Render the solid
